7c16a226a52c8ceb6ba6b225e1f5430305cdd315
Riyomi <sz.richard98@gmail.com>
2020-12-14 20:58:04 +0100

    forms

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 
-const Input = ({ name, label, value, onChange, autoFocus, type }) => {
+const Input = ({ name, label, error, ...rest }) => {
   return (
     <div className="form-group">
       <label htmlFor={name}>{label}</label>
-      <input
-        value={value}
-        onChange={onChange}
-        id={name}
-        autoFocus={autoFocus}
-        name={name}
-        type={type}
-        className="form-control"
-      />
+      <input {...rest} name={name} id={name} className="form-control" />
+      {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
-};
-
-Input.defaultProps = {
-  type: "text",
 };
 
 export default Input;
